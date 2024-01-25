c28c397aa3b6537f28f73a8bb5bc1d96669f1bbb
Swagath reddy <125737432+swagathreddy@users.noreply.github.com>
2024-01-26 01:29:59 +0530

    Add files via upload

---
+++ transitXpress/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+from io import BytesIO
+import qrcode
+import uuid
+from django.core.files.base import ContentFile
+from django.core.exceptions import ValidationError
+
+class Feature(models.Model):
+    bus_number = models.CharField(max_length=10, default='TS08AA0000')
+    bus_name = models.CharField(max_length=100, default='RTC')
+    ticket_fare = models.DecimalField(max_digits=8, decimal_places=2, default=50)
+    arrival_time = models.TextField(default='5.00')
+    fromdesti = models.CharField(max_length=100, default=None)
+    todesti = models.CharField(max_length=500, default=None)
+    place_notes = models.TextField(blank=True, null=True, default="Interesting place to visit")
+    bus_image = models.ImageField(upload_to='bus_images/', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.bus_number} - {self.bus_name} - {self.fromdesti} - {self.todesti}'
+
+class Confirmation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    from_location = models.CharField(max_length=255, null=True, blank=True)
+    to_location = models.CharField(max_length=255, null=True, blank=True)
+    passenger_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    payment_mode = models.CharField(max_length=20, null=True, blank=True, default=1)
+    booking_date = models.DateField(null=True, blank=True)
+    qr_code = models.ImageField(upload_to='qr_codes/', blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.qr_code:
+            qr_data = f"From: {self.from_location}\nTo: {self.to_location}"
+            qr_code_image = self.generate_qr_code(qr_data)
+
+            buffer = BytesIO()
+            qr_code_image.save(buffer, format='PNG')
+            image_bytes = buffer.getvalue()
+
+            # Save the QR code image to the qr_codes directory with a filename based on the instance's ID
+            self.qr_code.save(f"{self.id}_qr.png", ContentFile(image_bytes), save=False)
+
+        super().save(*args, **kwargs)
+
+    def generate_qr_code(self, data):
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(data)
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+        return img
+
+    def __str__(self):
+        return f"{self.passenger_name}'s Booking"
---
+++ transitXpress/views.py
@@ -0,0 +1,219 @@
+from django.shortcuts import render,redirect 
+from django.http import HttpResponse
+from .models import Feature,Confirmation
+from django.contrib import auth,messages
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail,EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from websiters.settings import EMAIL_HOST_USER
+from django.shortcuts import get_object_or_404
+from datetime import datetime
+from django.http import JsonResponse
+from django.core.files.base import ContentFile
+import qrcode,io
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def register(request):
+    if request.method=='POST':
+         username=request.POST['username']
+         email=request.POST['email']
+         password=request.POST['password']
+         password2=request.POST['password2']
+         if password==password2:
+             if User.objects.filter(email=email).exists():
+                 messages.info(request,'Email already exists')
+                 return redirect('register')
+             elif User.objects.filter(username=username).exists():
+                 messages.info(request,'username already taken')
+                 return redirect('register')
+             else:
+                 user =User.objects.create_user(username=username,
+                                                email=email,password=password)
+                 user.save()
+                 return redirect('login')
+         else:
+             messages.info(request,'passwords mismatch')
+             return render(request,'register.html')
+    return render(request,'register.html')
+
+
+
+def login(request):
+    if request.method=='POST':
+        username= request.POST['username']
+        password=request.POST['password']
+        user=auth.authenticate(username=username,password=password)
+        
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,"invalid credentials")
+            return redirect('login')
+    else:
+        return render(request,'login.html')
+    
+def style(request):
+    return render(request,'style.css')
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+from django.db.models import Q
+def DestinationDetails(request):
+    if request.method == 'GET':
+        from_location=request.GET.get('from_location')
+        to_location=request.GET.get('to_location')
+        booking_date_str=request.GET.get('bookingdate')
+        booking_date = datetime.strptime(booking_date_str, "%Y-%m-%d").date() if booking_date_str else None
+        booking_date_str = str(booking_date)
+        request.session['booking_date'] = booking_date_str
+        print(from_location,to_location,booking_date)
+        # print(f'From: {from_location}, To: {to_location}')
+        routes=Feature.objects.filter(Q(fromdesti__icontains=from_location) | Q(todesti__icontains=to_location))
+        return render(request, 'Destinationdetails.html',{'routes':routes})
+    messages.info(request,"No routes found. Please search for other places.")
+    return render(request, 'Destinationdetails.html', {'routes': []})    
+        #  if query and query1:
+    
+        #     routes=Feature.objects.filter(Q(fromdesti__icontains=query) | Q(todesti__icontains=query1))
+        
+        # if routes:   
+        #     return render(request, 'Destinationdetails.html',{'routes':routes})
+        # else:
+        #     messages.info(request,"No routes found. Please search for other places.")
+        #     return render(request, 'Destinationdetails.html', {'routes': []})
+
+@login_required
+def user_confirmation(request, route_id):
+    route = get_object_or_404(Feature, pk=route_id)
+    return render(request, 'userconfirmation.html', {'fromdesti': route.fromdesti, 'todesti': route.todesti
+                                                     ,'booking_date': request.session.get('booking_date')})
+
+@login_required
+def confirmation(request):
+    
+    if request.method == 'POST':
+        print(request.POST)
+        from_location = request.POST.get('fromdesti')
+        to_location = request.POST.get('todesti')
+        passenger_name = request.POST.get('passenger_name')
+        email = request.POST.get('email')
+        payment_mode = request.POST.get('payment_mode')
+        booking_date = request.session.get('booking_date')
+        
+        print(passenger_name,email,payment_mode,f'Fromdesti: {from_location}, Todesti: {to_location}')
+        # Assuming the user is logged in
+        # user=request.user
+        unique_code = f"{request.user}_{from_location}_{to_location}"
+        # Save booking details to the database
+        confirmation_obj= Confirmation.objects.create(
+            # user_id_id=.user,
+            user=request.user,
+            from_location=from_location,
+            to_location=to_location,
+            passenger_name=passenger_name,
+            email=email,
+            payment_mode=payment_mode,
+            booking_date=booking_date,
+            
+        )
+        # subject = 'Ticket Booking Confirmation'
+        # html_message = render_to_string('booking_confirmation_email.html', {
+        #     'passenger_name': passenger_name,
+        #     'from_location': fromdesti,
+        #     'to_location': todesti,
+        # })
+        # plain_message = strip_tags(html_message)
+        # from_email = 'kasulaswagreddy4296@gmail.com'  # Replace with your email
+        # to_email = [email]
+
+        # send_mail(subject, plain_message, from_email, to_email, html_message=html_message)
+        send_mail("Thankyou for your Booking",
+                  f"{passenger_name} your Ticket is successfully booked from {from_location} to {to_location} on {booking_date} Here is your QR CODE ",
+                  EMAIL_HOST_USER,
+                  [email],
+                  fail_silently=True, 
+                  html_message=f'<p>{passenger_name}, your Ticket is successfully booked from {from_location} to {to_location} on {booking_date}. Here is your QR CODE:</p><br><img src="cid:qr_code_image" alt="QR Code">'
+)
+        qr_code_data = f"Ticket Code: {unique_code}\nFrom: {from_location}\nTo: {to_location}"
+        qr_code_image = generate_qr_code(qr_code_data)
+        
+        buffer = io.BytesIO()
+        qr_code_image.save(buffer, format='PNG')
+        image_bytes = buffer.getvalue()
+        
+        confirmation_obj.qr_code.save(f"{unique_code}.png", ContentFile(image_bytes), save=False)
+        confirmation_obj.save()
+        
+        msg = EmailMultiAlternatives(
+    "Thank you for your Booking",
+    f"{passenger_name} your Ticket is successfully booked from {from_location} to {to_location} on {booking_date}. Here is your QR CODE ",
+    EMAIL_HOST_USER,
+    [email],
+)
+    msg.attach_alternative(f'<img src="cid:qr_code_image" alt="QR Code">', "text/html")
+    msg.mixed_subtype = 'related'  # Required for cid to work
+
+# Attach the image
+    msg.attach_file(confirmation_obj.qr_code.path, mimetype='image/png')
+    msg.send()
+        
+    return render(request, 'success.html', {
+    'from_location': from_location,
+    'to_location': to_location,
+    'passenger_name': passenger_name,
+    'email': email,'booking_date': booking_date,
+    'confirmation_obj': confirmation_obj,
+})
+# Replace with the actual template name
+    
+    if request.user.is_authenticated:
+        # If the user is already authenticated, proceed with the booking
+        return render(request, 'conformation.html')
+    messages.info(request, "Please create an account or log in to your account.")
+    return render(request, 'register.html')
+
+
+# Example using print statements
+
+
+def tickets_view(request):
+    # Logic to fetch user tickets from the database
+    # Replace this with your actual logic
+    
+    if request.user.is_authenticated:
+        user_tickets = Confirmation.objects.filter(user=request.user)
+        if user_tickets:
+            return render(request, 'tickets.html', {'user_tickets': user_tickets, 'user': request.user})
+        else:
+             messages.info(request,"No Tickets found")
+             return render(request, 'tickets.html' )
+    else:
+       return render(request, 'tickets.html' , {'user': request.user})
+        
+def routes(request):
+    features=Feature.objects.all()
+    return render(request, 'routes.html',{'features':features})
+def parcel_delivery(request):
+    return render(request,'parcel_delivery.html')
+
+
+
+def generate_qr_code(data):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    return img
