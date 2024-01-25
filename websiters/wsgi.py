65512c678c67c62945cba10715b689ee30ae0d0c
Swagath reddy <125737432+swagathreddy@users.noreply.github.com>
2024-01-26 02:38:13 +0530

    wsgi.py

--- websiters/wsgi.py
+++ websiters/wsgi.py
@@ -11,9 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from decouple import config
 
-config.read_env()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'websiters.settings')
 
