de29092df33e38d12b763583ab0c5d79453b9091
Swagath reddy <125737432+swagathreddy@users.noreply.github.com>
2024-01-26 07:50:34 +0530

    models.py

--- transitXpress/models.py
+++ transitXpress/models.py
@@ -29,39 +29,7 @@ class Confirmation(models.Model):
     email = models.EmailField()
     payment_mode = models.CharField(max_length=20, null=True, blank=True, default=1)
     booking_date = models.DateField(null=True, blank=True)
-    qr_code = models.ImageField(upload_to='dummy_folder/',null=True, blank=True)
     
-    def generate_qr_code(self, data):
-        qr = qrcode.QRCode(
-            version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
-            border=4,
-        )
-        qr.add_data(data)
-        qr.make(fit=True)
-
-        img = qr.make_image(fill_color="black", back_color="white")
-        return img
-    
-    def  save_qr_code_to_database(self):
-        if not self.qr_code:
-            qr_data = f"From: {self.from_location}\nTo: {self.to_location}"
-            qr_code_image = self.generate_qr_code(qr_data)
-
-            # Create a BytesIO object
-            buffer = BytesIO()
-
-            # Save the QR code image to the BytesIO object
-            qr_code_image.save(buffer, format='PNG')
-
-            # Set the image field of the model using the BytesIO object
-            self.qr_code.save(f"{self.id}_qr.png", SimpleUploadedFile(f"{self.id}_qr.png", buffer.getvalue()), save=False)
-
-        
-    def save(self, *args, **kwargs):
-        self.save_qr_code_to_database()
-        super().save(*args, **kwargs)
     
     def __str__(self):
         return f"{self.passenger_name}'s Booking"
