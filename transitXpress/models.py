9646f37ec5c29bb31e6dd6d2cade21fc233f5fcf
Swagath reddy <125737432+swagathreddy@users.noreply.github.com>
2024-01-27 23:11:04 +0530

    models.py

--- transitXpress/models.py
+++ transitXpress/models.py
@@ -29,7 +29,31 @@ class Confirmation(models.Model):
     email = models.EmailField()
     payment_mode = models.CharField(max_length=20, null=True, blank=True, default=1)
     booking_date = models.DateField(null=True, blank=True)
-    
+    qr_code_image = models.BinaryField(null=True, blank=True)
     
     def __str__(self):
         return f"{self.passenger_name}'s Booking"
+    
+    def generate_qr_code_image(self):
+        unique_code = f"{self.user}_{self.from_location}_{self.to_location}"
+        qr_code_data = f"Ticket Code: {unique_code}\nFrom: {self.from_location}\nTo: {self.to_location}"
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(qr_code_data)
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+        buffer = BytesIO()
+        img.save(buffer, format="PNG")
+
+        # Convert the image to binary data
+        binary_data = buffer.getvalue()
+        self.qr_code_image = binary_data
+
+    def save(self, *args, **kwargs):
+        self.generate_qr_code_image()
+        super().save(*args, **kwargs)
